2ae126b99093cf53fb9c5d5af065a09854f1f9b6
ATLAS2002 <siddharth211102@gmail.com>
2024-09-04 04:34:20 +0530

    fixed shit

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,35 +1,17 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import * as esprima from "esprima";
-import estraverse from "estraverse";
 
 export async function scrapeUrl(url: string) {
-  axios.request({ method: "GET", url }).then((response) => {
-    const $ = cheerio.load(response.data);
-    const scriptElem = [...$("body script")].find(
-      (elem, i) => {
-        return i === 3;
-      }
-    );
-    // console.log($(scriptElem).text().substring(0, 100));
-    const ast = esprima.parseScript($(scriptElem).text());
-    // console.log(ast.loc);
-    estraverse.traverse(ast, {
-      enter: function (node, parent) {
-        if (
-          node.type == "FunctionExpression" ||
-          node.type == "FunctionDeclaration"
-        )
-          return estraverse.VisitorOption.Skip;
-      },
-      leave: function (node, parent) {
-        if (node.type == "VariableDeclarator")
-          console.log(node.id);
-      },
-    });
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+  const scriptElem = [...$("body script")].find((_, i) => {
+    return i === 3;
   });
 
-  return Promise.resolve(
-    '[{"name": "John Doe", "age": 30, "city": "New York"}, {"name": "Jane Doe", "age": 25, "city": "San Francisco"}]'
-  );
+  const data = $(scriptElem)
+    .text()
+    .match(/\[\[.*\]\]/gm) as Array<string>;
+  console.log(data[0]);
+
+  return data[0];
 }
